17b2057e264f920bd1941fa2cf68906e756233ed
Faiz Khan <faiz.khan@briskteq.com>
2024-06-25 13:01:47 +0530

    feat: message box and chatbot updated

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,40 +1,27 @@
-import { Dispatch, SetStateAction } from "react";
-import { JSX as JSX_2 } from "react/jsx-runtime";
+import { Dispatch } from 'react';
+import { JSX as JSX_2 } from 'react/jsx-runtime';
+import { SetStateAction } from 'react';
 
-declare function Chatbot({
-  modal,
-  messages,
-  setMessages,
-  className,
-  title,
-  botMessageClassName,
-  userMessageClassName,
-  botMessageNotchClassName,
-  userMessageNotchClassName,
-  getResponse,
-  loading,
-  setLoading,
-  handleStopStream,
-}: {
-  modal?: boolean;
-  messages: MessageType[];
-  setMessages: Dispatch<SetStateAction<MessageType[]>>;
-  title: string;
-  loading: boolean;
-  setLoading: Dispatch<SetStateAction<boolean>>;
-  className?: string;
-  botMessageClassName?: string;
-  userMessageClassName?: string;
-  botMessageNotchClassName?: string;
-  userMessageNotchClassName?: string;
-  getResponse: () => void;
-  handleStopStream: () => void;
+declare function Chatbot({ modal, messages, setMessages, className, title, botMessageClassName, userMessageClassName, botMessageNotchClassName, userMessageNotchClassName, getResponse, loading, setLoading, handleStopStream, }: {
+    modal?: boolean;
+    messages: MessageType[];
+    setMessages: Dispatch<SetStateAction<MessageType[]>>;
+    title: string;
+    loading: boolean;
+    setLoading: Dispatch<SetStateAction<boolean>>;
+    className?: string;
+    botMessageClassName?: string;
+    userMessageClassName?: string;
+    botMessageNotchClassName?: string;
+    userMessageNotchClassName?: string;
+    getResponse: () => void;
+    handleStopStream: () => void;
 }): JSX_2.Element;
 export default Chatbot;
 
 declare type MessageType = {
-  source: "bot" | "user";
-  text: string;
+    source: "bot" | "user";
+    text: string;
 };
 
-export {};
+export { }
